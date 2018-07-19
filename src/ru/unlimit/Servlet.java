02cae96442a5580348d473174d2221c658a9c77c
alienshooter88 <41442700+alienshooter88@users.noreply.github.com>
2018-07-19 18:41:51 +0300

    Initial commit

---
+++ src/ru/unlimit/Servlet.java
@@ -0,0 +1,23 @@
+package ru.unlimit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        request.setAttribute("username", "John");
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Alien
+  Date: 10.07.2018
+  Time: 15:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Веб приложение</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <div class="container">
+  <div class="test">
+    <p>${requestScope.username}</p>
+  </div>
+  </div>
+  </body>
+</html>
